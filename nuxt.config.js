abd0facb66cfad6b90e9f6b1e591380769be2b99
serdarkok <kokserdal@gmail.com>
2019-08-26 18:08:39 +0300

    İlk Çalışma

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+        // Load a Node.js module directly (here it's a Sass file)
+        'bootstrap-scss',
+        // CSS file in the project
+        '@/assets/css/bundle.css',
+        // SCSS file in the project
+        //'@/assets/scss/bootstrap.scss'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/login/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="login-wrap">
+    <div class="login-box">
+      <form @submit.prevent="getLogin" method="POST">
+        <div class="form-group">
+          <label for="mail">E-Posta / Kullanıcı Adı</label>
+          <input
+            type="text"
+            v-model="mail"
+            class="form-control"
+            id="mail"
+            name="mail"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div class="form-group">
+          <label for="password">Şifre</label>
+          <input
+            type="password"
+            v-model="password"
+            class="form-control"
+            id="password"
+            name="password"
+            placeholder="Şifre"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">Giriş Yap</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from '@nuxtjs/axios';
+export default {
+  data() {
+    return {
+      mail: "",
+      password: "",
+      errors: []
+    };
+  },
+
+  methods: {
+    getLogin() {
+        this.$axios
+        .post("http://localhost:3001/api/login", this.mail, this.password)
+        .then(result => {
+          console.log(result);
+        })
+        .catch(err => {
+          this.errors.push(err);
+        });
+    },
+    async fetchSomething() {
+      const ip = await this.$axios.$get("http://icanhazip.com");
+      this.ip = ip;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
